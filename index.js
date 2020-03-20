7b995803fe700dc48bea44164bd67bf3244c13a4
Cesar GP <cgpcsjcmp@gmail.com>
2020-03-20 12:44:45 +0100

    ADD: New function FIX: Linting semicolon

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const addTwoNumbers = (a, b) => {
-  return a + b;
+  return a + b
+}
+const addThreeNumbers = (a, b, c) => {
+  return a + b + c
 }
 
 module.exports = addTwoNumbers
+module.exports = addThreeNumbers
