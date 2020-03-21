c303ef232b1d61ec4186684d9f6ba9d104f79ccf
Cesar GP <cgpcsjcmp@gmail.com>
2020-03-21 12:29:28 +0100

    FIX: Semistandard fixes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const addTwoNumbers = (a, b) => {
-  return a + b
-}
+  return a + b;
+};
 const addThreeNumbers = (a, b, c) => {
-  return a + b + c
-}
+  return a + b + c;
+};
 
-exports.addTwoNumbers = addTwoNumbers
-exports.addThreeNumbers = addThreeNumbers
+exports.addTwoNumbers = addTwoNumbers;
+exports.addThreeNumbers = addThreeNumbers;
